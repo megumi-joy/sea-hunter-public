d2f331748086a48c0b08b3f2e753615e0d3b97f7
Sea Hunter CI <ci@voicydroid.com>
2026-09-14 21:32:19 +0000

    Web export from megumi-joy/sea-hunter@a40e8ea946ace10ef4e5a673aed5e4c2f1603221

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789418413|35590139';
+const CACHE_VERSION = '1789421469|14593183';
 /** @type {string} */
 const CACHE_PREFIX = 'Sea Hunter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
